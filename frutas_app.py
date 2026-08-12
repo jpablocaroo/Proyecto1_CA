cf6290a998a218a80ec88b0e7473ed3291fe3bea
jpablocaroo <jcaroosorio38@gmail.com>
2026-08-12 13:22:32 -0500

    Update frutas_app.py

--- frutas_app.py
+++ frutas_app.py
@@ -19,7 +19,7 @@ st.write("Vector de tu fruta:", fruta_usuario)
 manzana = [170, 7.0, 7]
 banano = [120, 5.0, 9]
 naranja = [200, 8.0, 6]
-pera = [150, 5.0, 4]
+pera = [150, 6.0, 4]
 
 # Calculamos las distancias
 
